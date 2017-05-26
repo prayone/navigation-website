60970ce4670061919051f620e9dbc5eed659b139
prayone <1054864949@qq.com>
2017-05-26 14:37:08 +0800

    存图片

--- js/search.js
+++ js/search.js
@@ -8,18 +8,24 @@ var webCof = {
 	"wechat_so":"http://weixin.sogou.com/",
 	"github_so":"https://github.com/search?q=",
 	"shipin_so":"http://www.soku.com/search_video/?q=",
-	"yinyue_so":"http://music.hao123.com/",
+	"yinyue_so":"http://music.hao123.com/search?key=",
 	"wangpan_so":"http://www.panc.cc/"
 	}
+$(".search_web").on("click",searchWeb);//还有
+$(".bdsug").on('keyup',function(e){
+    if(e.keyCode === 13){
+        searchWeb();
+    }
+});
 function searchWeb(){
-	var keyWord = $('#bdSug').val();
-	var key = $('#myTab li.active').find('a').attr('dataurl');
+	var keyWord = $('.bdSug').val();
+	var key = $('.myTab li.active').find('a').attr('dataurl');
 	location.href = webCof[key]+keyWord;
 }
 function addqita(info){
 	var txt=$(info).text();
 	var dataurl=$(info).attr("dataurl");
-	debugger
-	$("#other_so").text(txt).attr("dataurl",dataurl);
-
+	$(".other_so").text(txt).attr("dataurl",dataurl);
+    $(".myTab li").removeClass("active");
+    $(".other_so").parent().addClass("active");
 }
